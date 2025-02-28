35b9bef827a1ac67f23f09d1c55ed40375abf549
n4wf3l <ajari.nawfel@hotmail.com>
2025-02-28 21:43:01 +0100

    refactor: ;

--- src/routes/_auth/statistics/index.tsx
+++ src/routes/_auth/statistics/index.tsx
@@ -188,7 +188,7 @@ function Statistics() {
           })}
         </div>
       </motion.div>
-      ;
+
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
