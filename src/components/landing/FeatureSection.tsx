13ef459f13e393cd9245a8bce4c069479806bb29
Mehdi Merkachi <merkachimehdi@gmail.com>
2025-03-06 00:06:50 +0100

    Fix feature section

--- src/components/landing/FeatureSection.tsx
+++ src/components/landing/FeatureSection.tsx
@@ -1,6 +1,3 @@
-import { Link } from "@tanstack/react-router";
-import { Button } from "../ui/button";
-import { motion } from "framer-motion";
 import { Bot, ChartColumn, Upload } from "lucide-react";
 
 const features = [
@@ -41,72 +38,43 @@ const features = [
 
 export default function FeatureSection() {
   return (
-    <motion.div
-      id="feature"
-      className="overflow-hidden bg-white py-24 sm:py-32"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.6 }}
-    >
-      <motion.div
-        className="px-16 lg:px-32"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-      >
-        <motion.div
-          className="mx-auto flex max-w-2xl flex-col lg:flex-row lg:max-w-none lg:items-center lg:gap-16"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.6, delay: 0.2 }}
-        >
-          <motion.div className="lg:w-1/2">
-            <h2 className="text-base font-semibold text-teal-600">
-              Elevate Your Futsal Experience
-            </h2>
-            <p className="mt-2 text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-              Streamlined Workflow for Futsal Clubs
-            </p>
-            <p className="mt-6 text-lg text-gray-600">
-              Transform the way your futsal club operates with our innovative
-              platform designed to enhance performance, analysis, and
-              engagement.
-            </p>
-            <motion.dl className="mt-10 space-y-8 text-base text-gray-600">
-              {features.map((feature) => (
-                <motion.div
-                  key={feature.name}
-                  className="relative pl-9"
-                  initial={{ opacity: 0, y: 10 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  transition={{ duration: 0.4 }}
-                >
-                  <dt className="inline font-semibold text-gray-900">
-                    {feature.icon}
-                    <span className="ml-3">{feature.name}</span>
-                  </dt>
-                  <dd className="mt-2 text-gray-600">{feature.description}</dd>
-                </motion.div>
-              ))}
-            </motion.dl>
-          </motion.div>
-
-          <motion.div
-            className="lg:w-1/2 flex justify-center"
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6, delay: 0.3 }}
-          >
-            <img
-              alt="PlayerTracker screenshot"
-              src="/PlayerTracker.jpeg"
-              width={480}
-              height={360}
-              className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem]"
-            />
-          </motion.div>
-        </motion.div>
-      </motion.div>
-    </motion.div>
-  );
+    <div id="feature" className="overflow-hidden bg-white py-24 mb-24 mt-24 sm:py-32">
+      <div className="mx-auto max-w-screen-2xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pt-4 lg:pr-8">
+            <div className="lg:max-w-2xl">
+              <h2 className="text-base/7 font-semibold text-teal-600">Elevate Your Futsal Experience </h2>
+              <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                Simplified Workflow for Futsal Clubs
+              </p>
+              <p className="mt-6 text-lg/8 text-gray-600">
+                Transform the way your futsal club operates with our innovative
+                platform designed to enhance performance, analysis, and
+                engagement.
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-900">
+                      {feature.icon}
+                      {feature.name}
+                    </dt>{' '}
+                    <dd className="inline">{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <img
+            alt="Product screenshot"
+            src="/Statistics.png"
+            width={2432}
+            height={1442}
+            className="w-[48rem] max-w-none rounded-xl ring-1 shadow-xl ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
+
